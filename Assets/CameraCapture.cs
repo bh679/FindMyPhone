a85ed40b59eecbfa103ce58f6090670c9d8735d3
Brennan Hatton <brennan@brennanhatton.com>
2024-04-23 14:48:17 +0800

    Photo taker in development

---
+++ Assets/CameraCapture.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using UnityEngine;
+ 
+public class CameraCapture : MonoBehaviour
+{
+	public int fileCounter;
+	public KeyCode screenshotKey;
+	public Camera Camera;
+ 
+	private void LateUpdate()
+	{
+		if (Input.GetKeyDown(screenshotKey))
+		{
+			Capture();
+		}
+	}
+ 
+	public void Capture()
+	{
+		RenderTexture activeRenderTexture = RenderTexture.active;
+		RenderTexture.active = Camera.targetTexture;
+ 
+		Camera.Render();
+ 
+		Texture2D image = new Texture2D(Camera.targetTexture.width, Camera.targetTexture.height);
+		image.ReadPixels(new Rect(0, 0, Camera.targetTexture.width, Camera.targetTexture.height), 0, 0);
+		image.Apply();
+		RenderTexture.active = activeRenderTexture;
+ 
+		byte[] bytes = image.EncodeToPNG();
+		Destroy(image);
+ 
+		File.WriteAllBytes(Application.dataPath + "/Snaps/" + fileCounter + ".png", bytes);
+		fileCounter++;
+	}
+}
--- Assets/_The Free Range Roombas/Scripts/Scanner.cs
+++ Assets/_The Free Range Roombas/Scripts/Scanner.cs
@@ -6,127 +6,128 @@ using UnityEngine.UI;
 
 public class Scanner : MonoBehaviour
 {
+	// Time required to recognize an item
 	public float recognizingLoadTime = 2f;
 	
+	// Currently scanning item
 	ItemDescription itemScanning;
 	
+	// UI text to display item information
 	public Text textDisplay;
+	// Source for playing audio clips
 	public AudioSource audioSource;
+	// Audio clips for scanning and scanned sounds
 	public AudioClip scanningClip, scanedClip;
+	// Event triggered at the start of the last story clip
 	public UnityEvent OnStartOfLastStoryClip;
 	
-	
+	// Array of story audio clips
 	public AudioClip[] storyClips;
+	// Frequency of skipping story clips
 	public int storyClipFrequency = 0;
-	//bool timeForStory = true;
+	// Current story clip index
 	int storyId = 0;
+	// Counter for skipped story clips
 	int storySkipId = 0;
 	
+	// Layer mask to define what objects the scanner can interact with
 	public LayerMask layerMask;
 	
+	// Maximum scanning distance
 	public float scanDistance = 5f;
+	// Time to wait between scans
 	public float scanReloadTime = 0f;
+	// Reference to the camera
 	public Camera camera;
 	
+	// Events triggered during scanning
 	public UnityEvent OnScanning, OnNotScanning;
+	// Events triggered when a scan is successful and when reading is finished
 	public UnityEvent OnScan = new UnityEvent(), OnScanReadFinish = new UnityEvent();
+	// UI image to show scanning animation
 	public Image scanningAnimationImage;
 	
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
+	
+	// Recognition progress
 	float recognizing = 0;
 	
+	// State flags for scanning
 	bool scanned = false;
 	bool readingScan = false;
-	// See Order of Execution for Event Functions for information on FixedUpdate() and Update() related to physics queries
+	
+	// FixedUpdate is called every physics step
 	void FixedUpdate()
 	{
-		
-		
+		// Exit if currently processing a reading scan
 		if(readingScan)
 			return;
-		// Bit shift the index of the layer (8) to get a bit mask
-		//int layerMask = 1 << 8;
 		
+		// Update the fill amount of the scanning animation based on recognition progress
 		if(scanningAnimationImage != null)
-			scanningAnimationImage.fillAmount = recognizing/recognizingLoadTime;
-		
+			scanningAnimationImage.fillAmount = recognizing / recognizingLoadTime;
 
-		// This would cast rays only against colliders in layer 8.
-		// But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
-		//layerMask = ~layerMask;
-
+		// Prepare for a raycast
 		RaycastHit hit;
-		// Does the ray intersect any objects excluding the player layer
+		// Perform the raycast forward from the scanner's position up to a certain distance
 		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, scanDistance, layerMask))
 		{
+			// Visualize the raycast in the scene view for debugging
 			Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-			//Debug.Log("Did Hit " + hit.collider.gameObject.name);
 			
+			// Attempt to get an ItemDescription component from the hit object
 			ItemDescription itemToScan = hit.collider.gameObject.GetComponent<ItemDescription>();
 			
+			// Process the item if found
 			if(itemToScan != null)
 			{
-				//Debug.Log("is item ");
+				// Check if the same item is still under scan
 				if(itemScanning == itemToScan)
 				{
+					// Check if the item has not been fully scanned yet
 					if(!scanned)
 					{
-						recognizing+= Time.deltaTime;
-						
-						//Debug.Log("is recognizing ");
+						// Increase recognition progress
+						recognizing += Time.deltaTime;
+						// Trigger the scanning event
 						OnScanning.Invoke();
 						
+						// Check if the item has been recognized
 						if(recognizing > recognizingLoadTime)
 						{
-							//scan success sound
+							// Play scanned sound
 							audioSource.PlayOneShot(scanedClip);
 							scanned = true;
 							readingScan = true;
+							// Trigger scan success event
 							OnScan.Invoke();
+							// Temporarily disable the camera
 							camera.gameObject.SetActive(false);
 							
 							float readtime;
 							
-							//if 
-							if(
-								//there are storyclips left to read 
-							(storyId < storyClips.Length)
-								//and we are not waiting to to skip it
-								&& storySkipId >= storyClipFrequency
-							)
+							// Determine if a story clip should be played
+							if(storyId < storyClips.Length && storySkipId >= storyClipFrequency)
 							{
-								//read story clip
+								// Play the current story clip
 								audioSource.PlayOneShot(storyClips[storyId]);
 								
-								//if last story clip
+								// If it is the last story clip, trigger the corresponding event
 								if(storyId == storyClips.Length - 1)
 									OnStartOfLastStoryClip.Invoke();
 								
-								//wait for clip to finish
+								// Set the read time based on the audio clip length
 								readtime = storyClips[storyId].length;
 								
-								//reset story skip id
+								// Reset skip counter and increment story clip index
 								storySkipId = 0;
-								
-								//increase story clip id
 								storyId++;
-								
-							}else
+							}
+							else
 							{
-								//actually scan the item
+								// Increment skip counter and scan the item
 								storySkipId++;
 								
+								// Start reading the scanned item
 								readtime = itemToScan.Scan(textDisplay, audioSource);
 								
 							}
@@ -167,57 +168,4 @@ public class Scanner : MonoBehaviour
 	{
 		scanReloadTime = newTime;
 	}
-    
-	/*void OnTriggerEnter(Collider collider)
-	{
-		ItemDescription newItemToScan = this.collider.gameObject.GetComponent<ItemDescription>();
-		
-		//if it is an item to scan
-		if(newItemToScan != null)
-		{
-			//get distance to item
-			float distance = Vector3.Distance(originForDistance.position,newItemToScan.transform.position);
-			
-			//make keypair value
-			KeyValuePair itemPair = new KeyValuePair<ItemDescription, float>(newItemToScan,distance);
-			
-			//put in right position
-			for(int i = 0 ; i < itemsInView.Count; i++)
-			{
-				if(itemsInView[i].Value > distance)
-				{
-					itemsInView.Insert(0,itemPair);
-				}
-				
-				if(i == 0)
-				{
-					recognizing = 0;
-				}
-			}
-		}
-		
-	}
-    
-	void OnTriggerStay(Collider collider)
-	{
-		ItemDescription newItemToScan = this.collider.gameObject.GetComponent<ItemDescription>();
-		
-		//if it is an item to scan
-		if(newItemToScan != null)
-		{
-			
-		}
-		recognizing+= Time.deltaTime;
-		
-		
-		if(recognizing > recognizingLoadTime)
-		{
-			ScanItem();
-		}
-	}
-	
-	void OnTriggerExit(Collider collider)
-	{
-		recognizing = 0
-	}*/
 }
